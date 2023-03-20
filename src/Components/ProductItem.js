577f42b8e0d1595663fe34b186d9c6a5fedb2e7b
Ankit Tomar <ankitjdec@gmail.com>
2023-03-20 11:09:11 +0530

    useHistory is replaced with the useNavigate

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const ProductItem = () => {
+  const history = useNavigate();
+  const handelRedirection = () => {
+    if (history) {
+      history("/product", { replace: true });
+    }
+  };
   return (
     <div>
       <h1>Product Item 1</h1>
+      <button onClick={handelRedirection}>Go Back</button>
     </div>
   );
 };
